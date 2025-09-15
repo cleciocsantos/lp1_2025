e42cdb643a13161cde1b24238c5ee42d3d3fae18
Clécio <santosc.clecio@gmail.com>
2025-09-15 18:21:14 -0300

    Aula do dia 15/09/2025 - acessando índice de lista

--- listas/media_lista.py
+++ listas/media_lista.py
@@ -5,4 +5,6 @@ while x < len(notas):
     soma += notas[x]
     x += 1
 media = soma/len(notas)
-print(f'A média é {media}')
+print(f'A média é {media:.1f}')
+posicao = int(input("Qual nota você deseja saber (de 1 a 7)? "))
+print(f"A {posicao}º nota é {notas[posicao-1]}")
