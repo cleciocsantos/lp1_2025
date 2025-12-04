71b096abfa165119be3c4103648de19c156a9c69
Clécio <santosc.clecio@gmail.com>
2025-12-04 15:43:09 -0300

    Aula de 04/12/2025 - ajustes no HTML

--- segunda_certificacao/conteudos_de_aulas/arquivos/filmes_html.py
+++ segunda_certificacao/conteudos_de_aulas/arquivos/filmes_html.py
@@ -6,14 +6,15 @@ filmes = {
 }
 with open("filmes.html", "w", encoding="utf-8") as pagina:
     pagina.write("""
-        <html>
-        <head>
-            <title>Filmes</title>
-        </head>
-        <body>
-    """)
+    <html>
+    <head>
+        <title>Filmes</title>
+    </head>
+    <body>""")
     for genero, nomes in filmes.items():
-        pagina.write(f"<h1>{genero}</h1>")
+        pagina.write(f"\n\t\t<h1>{genero}</h1>\n") # o caractere especial '\t' ajusta a tabulação (identação)
+        pagina.write(f"\t\t<ul>\n") # o caractere especial '\n' quebra a linha
         for filme in nomes:
-            pagina.write(f"<p>{filme}</p>")
-    pagina.write("</body></html>")
+            pagina.write(f"\t\t\t<li>{filme}</li>\n")
+        pagina.write(f"\t\t</ul>\n")
+    pagina.write("\t</body>\n\t</html>")
